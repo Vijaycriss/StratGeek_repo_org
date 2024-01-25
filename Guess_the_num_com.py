1e3b459ce6e71206eb143012450db4d533fb31cb
Vijay s <117348270+Vijaycriss@users.noreply.github.com>
2024-01-25 23:45:32 +0530

    Update Guess_the_num_com.py

--- Guess_the_num_com.py
+++ Guess_the_num_com.py
@@ -5,6 +5,7 @@ def guess(x):
     random_number = random.randint(1,x)
     guess = 0
     step_count = 0
+    step= 0
     while(guess != random_number):
         guess = int(input(f"Guess a number between 1 to {x}: "))
         list.append(guess)
